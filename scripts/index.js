88c5ec2f9a3a227f41e538171fee3d31cb379924
Алексей Сергеевич Куров <alexei.s.kurov@gmail.com>
2022-06-03 19:26:44 +0300

    first review: bugs are fixed

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,6 @@
 //находим кнопки в DOM
 let buttonEdit = document.querySelector('.profile__edit-button');
-let buttonAdd = document.querySelector('.profile__add-button');
-let buttonLike = document.querySelector('.card__button-like');
 let buttonClose = document.querySelector('.popup__button-close');
-let buttonSave = document.querySelector('.popup__button-save');
 
 //находим popup в DOM
 let popup = document.querySelector('.popup');
@@ -18,47 +15,29 @@ let jobInput = formElement.querySelector('.popup__input_type_about');
 let nameProfile = document.querySelector('.profile__name');
 let aboutProfile = document.querySelector('.profile__about');
 
-//функция сохранения результата ввода в popup
-function saveResult() {
-    nameProfile.textContent = nameInput.value;
-    aboutProfile.textContent = jobInput.value;
-}
-
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
-function formSubmitHandler (evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-    // Так мы можем определить свою логику отправки.
-    // О том, как это делать, расскажем позже.
-    saveResult();
-}
-
 //функция открывающая/закрывающая popup
 function popupSwitch() {
     popup.classList.toggle('popup_opened');
 }
 
-//обработчик нажатия клавиш
-function keyPressed(evt) {
-    if(evt.code === 'Escape') {
-        popupSwitch();
-        document.removeEventListener('keydown',keyPressed);      
-    } 
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function formSubmitHandler(evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+    // Так мы можем определить свою логику отправки.
+    // О том, как это делать, расскажем позже.
+    nameProfile.textContent = nameInput.value;
+    aboutProfile.textContent = jobInput.value;
+    popupSwitch();
 }
 
 // обработчик нажатия кнопок редактирования профиля и сохранить/закрыть
-function profileEdit(evt) { 
-    popupSwitch(); 
-    //popup открылся   
-    if(popup.classList.contains('popup_opened')) {  
-        document.addEventListener('keydown',keyPressed);
+function profileEdit() {
+    popupSwitch();
+    //если popup открылся   
+    if (popup.classList.contains('popup_opened')) {
         nameInput.value = nameProfile.textContent;
         jobInput.value = aboutProfile.textContent;
-    } else { //popup закрылся
-        document.removeEventListener('keydown',keyPressed);
-        if(evt.currentTarget.classList.contains('popup__button-save')) {
-            saveResult();
-        }
     }
 }
 
@@ -69,4 +48,3 @@ formElement.addEventListener('submit', formSubmitHandler);
 //прикрепляем обработчики к кнопкам
 buttonEdit.addEventListener('click', profileEdit);
 buttonClose.addEventListener('click', profileEdit);
-buttonSave.addEventListener('click', profileEdit);
